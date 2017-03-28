c408c55b8f64b2d292eedea809ec834a04897547
Jeet Hathiwala <jhathiwala@gmail.com>
2017-03-28 18:25:09 +0530

    Implemented a basic navigation bar

---
+++ app/src/main/java/com/airavata/sihemployeemanagement/MainActivity.java
@@ -0,0 +1,34 @@
+package com.airavata.sihemployeemanagement;
+
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Set the toolbar to replace the deprecated actionbar
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        // Setup the navigation drawer
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.nav_drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.nav_drawer_open, R.string.nav_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        // Set the initial fragment
+        FragmentManager manager = getSupportFragmentManager();
+        FragmentTransaction transaction = manager.beginTransaction();
+        transaction.replace(R.id.main_activity_fragment_container, LoginFragment.newInstance());
+        transaction.commit();
+    }
+}
