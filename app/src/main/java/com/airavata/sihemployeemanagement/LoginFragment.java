08b9bbe3fe4b332af8fe8680318a92a9d10eb0d1
Jeet Hathiwala <jhathiwala@gmail.com>
2017-03-28 23:11:21 +0530

    Implemented the Login UI

--- app/src/main/java/com/airavata/sihemployeemanagement/LoginFragment.java
+++ app/src/main/java/com/airavata/sihemployeemanagement/LoginFragment.java
@@ -3,17 +3,17 @@ package com.airavata.sihemployeemanagement;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
-
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link LoginFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class LoginFragment extends Fragment {
+    TextView registerAccountTv;
+    Button loginBtn;
 
     public LoginFragment() {
         // Required empty public constructor
@@ -31,7 +31,28 @@ public class LoginFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false);
+        View view = inflater.inflate(R.layout.fragment_login, container, false);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        // Initialize elements
+        registerAccountTv = (TextView) getActivity().findViewById(R.id.fragment_login_register_link);
+        loginBtn = (Button) getActivity().findViewById(R.id.fragment_login_submit);
+
+        // Navigate to the register fragment if the user doesn't have an account.
+        registerAccountTv.setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View view) {
+                FragmentManager manager = getActivity().getSupportFragmentManager();
+                FragmentTransaction transaction = manager.beginTransaction();
+                transaction.setCustomAnimations(android.R.anim.fade_in, android.R.anim.fade_out); // Set the anims
+                transaction.addToBackStack(this.getClass().getName());
+                transaction.replace(R.id.main_activity_fragment_container, RegisterFragment.newInstance());
+                transaction.commit();
+
+            }
+        });
     }
 
 }
---
+++ app/src/main/java/com/airavata/sihemployeemanagement/RegisterFragment.java
@@ -0,0 +1,31 @@
+package com.airavata.sihemployeemanagement;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class RegisterFragment extends Fragment {
+    public RegisterFragment() {
+        // Required empty public constructor
+    }
+
+    public static RegisterFragment newInstance() {
+        return new RegisterFragment();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_register, container, false);
+    }
+
+}
